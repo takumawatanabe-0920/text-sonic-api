a16e460335f0317799f2f28919c0b4e5e2bfc48c
takuma <takumaozk0920@gmail.com>
2023-11-08 02:12:38 -0800

    feat: type

--- app/main/speech_to_text/dto/response_dto.py
+++ app/main/speech_to_text/dto/response_dto.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel
 
 
+class TranscribeSpeechWordDto(BaseModel):
+    start: float
+    end: float
+    word: str
+
+
 class TranscribeResponseDto(BaseModel):
     audio_time: float
-    speech_word_list: list[
-        dict[
-            str,
-            float,
-        ]
-    ]
+    speech_word_list: list[TranscribeSpeechWordDto]
